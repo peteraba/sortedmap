31b1a7213e4592ca1222460a5afc1fbba5585ddd
Peter Aba <zsolt.peter.aba@zalando.de>
2024-12-08 21:13:00 +0100

    Minor improvements

--- sortedmap_test.go
+++ sortedmap_test.go
@@ -64,8 +64,8 @@ func TestSortedMap_Delete(t *testing.T) {
 	value1, value2, value3 := "value1", "value2", "value3"
 
 	sm := sortedmap.New[string, string]().
-		Set(key1, value1).
 		Set(key2, value2).
+		Set(key1, value1).
 		Set(key3, value3)
 
 	assert.True(t, sm.Has(key1))
@@ -85,8 +85,8 @@ func TestSortedMap_Keys(t *testing.T) {
 	expectedKeys := []string{key1, key2, key3}
 
 	sm := sortedmap.New[string, string]().
-		Set(key1, value1).
 		Set(key2, value2).
+		Set(key1, value1).
 		Set(key3, value3)
 
 	actual := sm.Keys()
@@ -163,7 +163,7 @@ func TestSortedMap_ParallelSet(t *testing.T) {
 }
 
 func TestSortedMap_ParallelSetFloatFloat(t *testing.T) {
-	key1, key2, key2b, key3 := 1.23, 2.34, 2.34, 3.45
+	key1, key2, key3 := 1.23, 2.34, 3.45
 	value1, value2, value2b, value3 := 8.34, 9.45, -9.45, 10.56
 
 	sm := sortedmap.New[float64, float64]()
@@ -171,19 +171,29 @@ func TestSortedMap_ParallelSetFloatFloat(t *testing.T) {
 	go func() {
 		time.Sleep(time.Duration(rand.Intn(500)) * time.Microsecond)
 
-		sm.Set(key1, value1).Set(key2, value2)
+		sm.Set(key1, value1)
+
+		time.Sleep(time.Duration(rand.Intn(500)) * time.Microsecond)
+
+		sm.Set(key2, value2)
 	}()
 
 	go func() {
 		time.Sleep(time.Duration(rand.Intn(500)) * time.Microsecond)
 
-		sm.Set(key2b, value2b).Set(key3, value3)
+		sm.Set(key3, value3)
+
+		time.Sleep(time.Duration(rand.Intn(500)) * time.Microsecond)
+
+		sm.Set(key2, value2b)
 	}()
 
 	time.Sleep(time.Millisecond)
 
 	assert.Equal(t, 3, sm.Len())
 	assert.Equal(t, []float64{key1, key2, key3}, sm.Keys())
+	assert.Equal(t, value1, sm.MustGet(key1))
+	assert.Equal(t, value3, sm.MustGet(key3))
 }
 
 func TestSortedMap_ParallelDelete(t *testing.T) {
@@ -230,4 +240,33 @@ func TestSortedMap_NewFrom(t *testing.T) {
 	assert.Equal(t, 1, sm.Len())
 	assert.Equal(t, []string{key1}, sm.Keys())
 	assert.Equal(t, []string{value1}, sm.Values())
+}
+
+type A struct {
+	B []string
+	C []int
+}
+
+func (a *A) AddB(newB string) *A {
+	a.B = append(a.B, newB)
+
+	return a
+}
+
+func TestSortedMap_NewFromComplex(t *testing.T) {
+	key1 := "key1"
+	value1 := &A{
+		B: []string{"b1", "b2"},
+		C: []int{1, 2},
+	}
+
+	sm := sortedmap.NewFrom(key1, value1)
+
+	sm.Set(key1, sm.MustGet(key1).AddB("b3"))
+
+	assert.Equal(t, 1, sm.Len())
+
+	actualValue := sm.MustGet(key1)
+	assert.Equal(t, []string{"b1", "b2", "b3"}, actualValue.B)
+	assert.Equal(t, []int{1, 2}, actualValue.C)
 }
